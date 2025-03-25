f5872271feb8c2f57d9c54095f9ec96849861480
Shulga Maxim <shulgamackim@gmail.com>
2025-03-25 12:56:07 +0200

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { Fragment as _Fragment, jsx as _jsx } from "react/jsx-runtime";
+import { useState } from 'react';
+import './App.css';
+function App() {
+    var _a = useState(0), count = _a[0], setCount = _a[1];
+    return (_jsx(_Fragment, { children: "asdasd" }));
+}
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import {defineConfig, loadEnv} from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr';
+import * as path from "path";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env': env,
+    },
+    plugins: [react(), svgr()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  };
+});
