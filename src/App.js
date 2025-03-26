e9e977c3c836a979882cf3a9af3fb3f8e5aaccf3
Shulga Maxim <shulgamackim@gmail.com>
2025-03-26 16:51:45 +0200

    fix

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import { Fragment as _Fragment, jsx as _jsx } from "react/jsx-runtime";
-import { useState } from 'react';
-import './App.css';
-function App() {
-    var _a = useState(0), count = _a[0], setCount = _a[1];
-    return (_jsx(_Fragment, { children: "asdasd" }));
-}
-export default App;
